8b0029cdb1dd7e8b5da02ac5205bba4b6826e604
Tatiana Ruiz <kellyruiz2209@gmail.com>
2023-10-10 18:53:51 -0500

    Version con login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from './Firebase/firebase';
+
 import './App.css';
+import { Login } from './Components/Login/Login';
+import { Navbar } from './Components/Navbar/Navbar';
 
 function App() {
+  const user = auth.currentUser
+  const submit = async () => { await signOut(auth) }
+
+
+  console.log(user)
   return (
     <>
+      {!user &&
+       <Navbar
+       state={false}
+       /> 
+      }
+
+      {user &&
+        <button onClick={submit}>Log out</button>
+      }
     </>
   );
 }
